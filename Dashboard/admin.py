f6f985a19aa6a60ad52622d7df7b2add136ec758
Ethan-the-coder <ngangaewilliam@gmail.com>
2026-02-25 13:55:56 +0300

    first commit

---
+++ Dashboard/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from Dashboard.models import Student
+from Dashboard.models import Exam
+# Register your models here.
+admin.site.register(Student)
+admin.site.register(Exam)
---
+++ Dashboard/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+# New students model
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    course =models.CharField(max_length=30)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=6,null=True)
+    email = models.EmailField()
+    date = models.DateField(null=True)
+
+    def __str__(self):
+        return self.name
+
+# Exam model
+class Exam(models.Model):
+    name = models.CharField(max_length=20)
+    exam_code = models.IntegerField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
